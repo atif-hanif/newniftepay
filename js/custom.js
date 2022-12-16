f0334ba6e2b7cf71ec0c8aa00b64c307ca0730d1
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-12-16 17:26:25 +0500

    16-12-2022 (1)

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,68 @@
 //Session
 
-var deadline = new Date("Dec 03, 2022 13:31:00").getTime();
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	var t = deadline - now;
-	var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-	// var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60 ));
-	var minutes = Math.floor(t / (1000 * 60 ));
-	var seconds = Math.floor((t % (1000 * 60)) / 1000);
-	document.getElementById("timer").innerHTML = "<div class='minutes'>" + minutes + "</div> : <div class='seconds'>" + seconds + '</div>';
-		if (t < 0) {
-			clearInterval(x);
-			document.getElementById("timer").innerHTML = "EXPIRED";
+// var deadline = new Date("Dec 03, 2022 13:31:00").getTime();
+// var x = setInterval(function() {
+// 	var now = new Date().getTime();
+// 	var t = deadline - now;
+// 	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+// 	var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+// 	// var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60 ));
+// 	var minutes = Math.floor(t / (1000 * 60 ));
+// 	var seconds = Math.floor((t % (1000 * 60)) / 1000);
+// 	document.getElementById("timer").innerHTML = "<div class='minutes'>" + minutes + "</div> : <div class='seconds'>" + seconds + '</div>';
+// 		if (t < 0) {
+// 			clearInterval(x);
+// 			document.getElementById("timer").innerHTML = "EXPIRED";
+// 		}
+// }, 1000);
+
+$(function() {
+	$('.counter').setCountDownTimer({
+		time: "00:05:00",
+		// button: $('.btn'),
+		countDownText: '',
+		afterCountText: 'Time\'s up!'
+	});
+});
+
+$.fn.setCountDownTimer = function (param) {
+	this.html('<label class="countdownText" style="float:left;margin-right:5px;">' + param.countDownText + '</label>' +
+			'<div id="countdown" style="float:left;">' +
+				'<strong>' +
+					'<span id="minutes" style="float:left"></span>' +
+					'<span style="float:left">:</span>' +
+					'<span id="seconds" style="float:left"></span>' +
+				'</strong>' +
+			'</div>' +
+			'<div id="aftercount" style="display:none">' + param.afterCountText + '</div>');
+	var remTime = param.time.split(":"),
+		sTime = new Date().getTime(),
+		countDown = (remTime[01] * 60) + parseInt(remTime[02]),
+		minutesSel = $("#minutes"),
+		secondsSel = $("#seconds"),
+		afterCountSel = $("#aftercount"),
+		countDownSel = $("#countdown"),
+		countDownTextSel = $('.countdownText');
+
+	var timer = setInterval(function () {
+		var cTime = new Date().getTime(),
+		diff = cTime - sTime,
+		seconds = countDown - Math.floor(diff / 1000);
+		//param.button.attr('disabled', 'disabled');
+		if (seconds >= 0) {
+			var minutes = Math.floor(seconds / 60);
+			seconds -= minutes * 60;
+			minutesSel.text(minutes < 10 ? "0" + minutes : minutes);
+			secondsSel.text(seconds < 10 ? "0" + seconds : seconds);
+		} else {
+			countDownSel.hide();
+			countDownTextSel.hide();
+			afterCountSel.show();
+			//param.button.removeAttr('disabled');
+			clearInterval(timer);
 		}
-}, 1000);
+	}, 00);
+}
 
 // Bank Form
 
